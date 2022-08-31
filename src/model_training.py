8540b7fb2549a681ae083b83918cab3ab36c56f0
anirudh-g <g.anirudh15@gmail.com>
2022-08-31 14:10:51 +0000

    model_training

---
+++ src/model_training.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn import compose
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn import ensemble
